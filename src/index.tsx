15de078fce044aa0f94df51b2eb6f923999b26b6
AmilMasic <amil.masic@gmail.com>
2021-10-14 13:11:14 -0600

    refactored code, moved onLoad from unpkg-path-plugin into it's own file, named fetch-plugin

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
 import { useState, useEffect, useRef} from 'react';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
+
 const App = () => {
   const ref = useRef<any>();
   const [input, setInput] = useState('');
@@ -26,7 +28,7 @@ const App = () => {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin(input)],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
         'process.env.NODE_ENV': '"production"',
         global: 'window'
