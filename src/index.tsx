ccf1561ed23f6f7afadca6c0805402fe8e89553e
AmilMasic <amil.masic@gmail.com>
2021-10-25 11:26:29 -0600

    moved iframe source into a variable, to load with srcDocs tag

--- src/index.tsx
+++ src/index.tsx
@@ -38,6 +38,12 @@ const App = () => {
     setCode(result.outputFiles[0].text);
   };
 
+  const html = `
+    <script>
+      ${code}
+    </script>
+  `;
+
   return (<div>
           <textarea
             value={input}
@@ -47,9 +53,11 @@ const App = () => {
             <button onClick={onClick}>Submit</button>
           </div>
           <pre>{code}</pre>
-          <iframe sandbox="" src="/test.html"/>
+          <iframe sandbox="allow-scripts" srcDoc={html} />
         </div>
   );
 };
+
+
 
 ReactDOM.render(<App />, document.querySelector('#root'));
