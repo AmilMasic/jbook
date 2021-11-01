8928592e7a109d006f99ce5ee81eb983083c5d8b
AmilMasic <amil.masic@gmail.com>
2021-11-01 16:14:32 -0600

    fixed dragging problems

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,4 +1,6 @@
 import { useRef, useEffect } from 'react';
+import './preview.css';
+
 interface PreviewProps {
   code: string;
 }
@@ -33,13 +35,17 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
     iframe.current.contentWindow.postMessage(code, '*');
   }, [code]);
 
-  return <iframe
-    style={{backgroundColor: 'white'}}
-    title="preview"
-    ref={iframe}
-    sandbox="allow-scripts"
-    srcDoc={html}
-  />
+  return (
+    <div className="preview-wrapper">
+      <iframe
+        style={{backgroundColor: 'white'}}
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+    </div>
+  )
 };
 
 export default Preview;
