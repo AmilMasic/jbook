6b1901c91df69089bcd82d41caa0bd709814d3fd
AMIL MASIC <amil.masic@gmail.com>
2021-11-03 11:08:58 -0600

    added useEffect with async timeout function for bundling in code-cell.tsx

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import { useState} from 'react';
+import { useState, useEffect } from 'react';
 import CodeEditor from './/code-editor';
 import Preview from './/preview';
 import bundle from '../bundler';
@@ -9,10 +9,12 @@ const CodeCell = () => {
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  useEffect(() => {
+    setTimeout(async () => {
+      const output = await bundle(input);
+    }, 1000);
+  }, [input]);
+
 
   return (
     < Resizable direction="vertical">
