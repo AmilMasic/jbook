95418d9c1584a1e04ab3a52e41a72535efe7d913
AMIL MASIC <amil.masic@gmail.com>
2021-11-09 16:53:13 -0700

    stubbed out action-creators/index.ts

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,1 +1,24 @@
-export {}
+import {ActionType} from '../action-types';
+import {
+  Action,
+  UpdateCellAction,
+  DeleteCellAction,
+  MoveCellAction,
+  InsertCellBeforeAction
+} from "../actions";
+
+export const = updateCell = (id: string, content: string): UpdateCellAction => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: {
+      id,
+      content
+    }
+  }
+};
+
+export const = deleteCell = (): DeleteCellAction => {};
+
+export const = moveCell = (): MoveCellAction => {};
+
+export const = insertCellBefore = (): InsertCellBeforeAction => {};
