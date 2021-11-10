df53c031382960ff148f3765711a25cf00d9382a
AmilMasic <amil.masic@gmail.com>
2021-11-10 13:56:23 -0700

    fixed minor issues

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -9,24 +9,24 @@ import {
 } from "../actions";
 import { CellTypes } from '../cell';
 
-export const = updateCell = (id: string, content: string): UpdateCellAction => {
+export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
     type: ActionType.UPDATE_CELL,
     payload: {
       id,
       content
-    };
+    },
   };
 };
 
-export const = deleteCell = (id: string): DeleteCellAction => {
+export const deleteCell = (id: string): DeleteCellAction => {
   return {
     type: ActionType.DELETE_CELL,
     payload: id,
   }
 };
 
-export const = moveCell = (id: string, direction: Direction): MoveCellAction => {
+export const moveCell = (id: string, direction: Direction): MoveCellAction => {
   return {
     type: ActionType.MOVE_CELL,
     payload: {
@@ -36,7 +36,7 @@ export const = moveCell = (id: string, direction: Direction): MoveCellAction => 
   };
 };
 
-export const = insertCellBefore = (id: string, cellType: CellTypes ): InsertCellBeforeAction => {
+export const insertCellBefore = (id: string, cellType: CellTypes ): InsertCellBeforeAction => {
   return {
     type: ActionType.INSERT_CELL_BEFORE,
     payload: {
