91551a0373159b060940080ae3fd9b041f72e5f2
AmilMasic <amil.masic@gmail.com>
2021-11-16 18:11:33 -0700

    wired up add-cell and cell-list and made necessary changes to make them work together

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,4 +1,6 @@
+import { Fragment } from 'react';
 import {useTypedSelector} from '../hooks/use-typed-selector';
+import AddCell from './add-cell';
 import CellListItem from './cell-list-item';
 
 const CellList: React.FC = () => {
@@ -8,8 +10,18 @@ const CellList: React.FC = () => {
     });
   });
 
-  const renderedCells = cells.map(cell => <CellListItem key={cell.id} cell={cell} />)
-  return <div>{renderedCells}</div>
+  const renderedCells = cells.map((cell) => (
+    <Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id}/>
+      <CellListItem cell={cell} />
+    </Fragment>
+  ));
+  return (
+    <div>
+      {renderedCells}
+      <AddCell nextCellId={null}/>
+    </div>
+  );
 };
 
 export default CellList;
