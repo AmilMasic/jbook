81bc8c6fa3ad1d8e7f506515625782d485680bf4
AmilMasic <amil.masic@gmail.com>
2021-11-23 17:21:12 -0700

    updated code cell to call cumulative code

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -35,17 +35,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join('\n');
       return;
     }
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content)
+      createBundle(cell.id, cumulativeCode.join('\n'));
     }, 1000);
     return () => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.content, cell.id, createBundle]);
+  }, [cumulativeCode.join('\n'), cell.id, createBundle]);
 
 
   return (
