ca4a4c66f9f71c0b037e5191fec66b27f3c069f0
AmilMasic <amil.masic@gmail.com>
2021-12-23 17:03:38 -0500

    wrapped the createCellsRouter in an export statement, added it to local-api/src/index.ts

--- jbook/packages/local-api/src/routes/cells.ts
+++ jbook/packages/local-api/src/routes/cells.ts
@@ -2,21 +2,26 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/cells', async (req, res) => {
-  // make sure the cell storage file exists
-  // if it does not exist, add in a default list of cells
+export const createCellsRouter = (filename: string, dir: string) => {
 
-  //read the file
-  // parse a list of cells out of it
-  // Send list of cells back to browser
-});
 
-router.post('/cells', async (req, res) => {
-  // make sure the file exists
+  router.get('/cells', async (req, res) => {
+    // make sure the cell storage file exists
+    // if it does not exist, add in a default list of cells
 
-  // if not, create it
+    //read the file
+    // parse a list of cells out of it
+    // Send list of cells back to browser
+  });
 
-  // take the list of cells from the request object
-  // serialize them
-  // write the cells into the file
-})
+  router.post('/cells', async (req, res) => {
+    // make sure the file exists
+
+    // if not, create it
+
+    // take the list of cells from the request object
+    // serialize them
+    // write the cells into the file
+  })
+  return router;
+};
