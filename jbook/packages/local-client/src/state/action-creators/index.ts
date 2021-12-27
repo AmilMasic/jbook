9bc5a65c8d183563f70559505e738ae0fb393938
AMIL MASIC <amil.masic@gmail.com>
2021-12-27 17:23:21 -0500

    created fetchCells function inside actionCreaters/index.ts

--- jbook/packages/local-client/src/state/action-creators/index.ts
+++ jbook/packages/local-client/src/state/action-creators/index.ts
@@ -1,4 +1,5 @@
 import { Dispatch} from 'redux';
+import axios from 'axios';
 import {ActionType} from '../action-types';
 import {
   Action,
@@ -8,7 +9,7 @@ import {
   InsertCellAfterAction,
   Direction
 } from "../actions";
-import { CellTypes } from '../cell';
+import {Cell, CellTypes } from '../cell';
 import bundle from '../../bundler';
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
@@ -68,3 +69,23 @@ export const createBundle = (cellId: string, input: string) => {
     })
   }
 };
+
+export const fetchCells = () => {
+  return () => {
+    return async (dispatch: Dispatch<Action>) => {
+      dispatch ({ type: ActionType.FETCH_CELLS});
+      try {
+        const { data }: {data: Cell[] } = await axios.get('/cells');
+
+        dispatch ({ type: ActionType.FETCH_CELLS_COMPLETE,
+          payload: data
+        });
+      } catch (err: any) {
+        dispatch({
+          type: ActionType.FETCH_CELLS_ERROR,
+          payload: err.message,
+        });
+      }
+    }
+  };
+}
